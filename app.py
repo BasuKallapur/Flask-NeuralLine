a9fbefb225c969ade704b81c3cea9daa8925bfab
BasuKallapur <basukallapur0@gmail.com>
2025-09-02 23:35:13 +0530

    modified files

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, request
 
 app= Flask(__name__)
 
 @app.route('/')
 def index():
-    return ("Hello Basavaraj")
+    return "Hello Basavaraj"
+
+# @app.route('/hello', methods= ['GET', 'POST'])
+# def hello():
+#     if request.method == 'GET':
+#         return 'you made a GET request \n'
+#     elif request.method == 'POST':
+#         return 'you made a GET request \n'
+#     return "you will never see this messege \n"   
+     
+@app.route('/hello')
+def hello():
+    return 'hello world!', 201      
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello {name} bro'
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return f'sum of {num1} + {num2} is {num1 + num2}'
+
+@app.route('/handle_url_params')
+def handle_params():
+    greeting= request.args['greeting']
+    name= request.args['name']
+    return f'{greeting}, {name}'
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000, debug=True)
+
---
+++ app2.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, url_for
+
+app= Flask(__name__, template_folder='templates')
+
+@app.route('/hello')
+def hello():
+    list= [1, 2, 3, 4]
+    return render_template('index.html', list=list)
+
+@app.route('/other')
+def other():
+    text= 'this is text'
+    return render_template('other.html', text= text)
+
+@app.route('/redirect')
+def redirect_endpoint():
+    return redirect(url_for('other'))
+
+# custom template filters
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1] # to reverse string
+
+@app.template_filter('repeat')
+def repeat(s, time= 2):
+    return s * time # to reverse string
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
